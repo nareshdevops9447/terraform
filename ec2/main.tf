82bc49c502ec5b679bae4428fccdb6bab2c84f96
nareshdocker9447 <nk122121@gmail.com>
2023-05-22 21:46:07 +0100

    ec2 with route53

---
+++ ec2/main.tf
@@ -0,0 +1,71 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["data.aws_caller_identity.current.account_id"]
+}
+
+
+resource "aws_instance" "ec2" {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  tags = {
+    Name = var.component
+  }
+}
+
+resource "null_resource" "provisioner" {
+  provisioner "remote-exec" {
+
+    connection {
+      host     = aws_instance.ec2.public_ip
+      user     = "centos"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "ansible-pull -i localhost, -U https://guthub.com/nareshdevops9447/terraform.git roboshop.yml -e role_name=${var.component}"
+      ]
+
+  }
+
+}
+
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z03132065F50QVUD6BFB"
+  name    = "${var.component}-dev.nareshlab.live"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+
+variable "component" {}
+variable "instance_type" {}
+variable "env" {
+  default = "dev"
+}
+variable "password" {}
