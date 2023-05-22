dd8ec222f117c61052d68f7002d4bb65a9c448e0
nareshdocker9447 <nk122121@gmail.com>
2023-05-22 21:54:39 +0100

    ec2 with route53

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
+  name_regex  = "ansible image"
   owners      = ["data.aws_caller_identity.current.account_id"]
 }
 
